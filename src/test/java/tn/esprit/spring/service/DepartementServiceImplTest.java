01f6e27a51c81bf9734639044c06421612a4e5a6
MounaKouki <mouna.kouki@esprit.tn>
2021-11-02 14:40:38 +0100

    validation

--- src/test/java/tn/esprit/spring/service/DepartementServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/DepartementServiceImplTest.java
@@ -2,7 +2,7 @@ package tn.esprit.spring.service;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import java.util.Date;
+
 import java.util.List;
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
